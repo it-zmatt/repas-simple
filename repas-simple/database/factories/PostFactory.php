583f9a96ca65430fe4c09d2c8cf22edf36bddf4c
it-zmatt <mohamed.taous@univ-khenchela.dz>
2024-07-23 10:34:49 -0400

    Created a table Post and seeded it with some values

---
+++ repas-simple/database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => $this->faker-> sentence(3),
+            "text" => $this->faker-> paragraph(3),
+            "post_id" => $this->faker-> numberBetween(1,10)
+
+        ];
+    }
+}
